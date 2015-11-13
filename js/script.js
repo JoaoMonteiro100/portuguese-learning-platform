ba06d5b4f49a0d4c51033be1f11a590881c8daed
João Monteiro <joaomiguelmonteiro@gmail.com>
2015-11-13 21:04:19 +0200

    Added help

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+function showHelp() {
+	document.getElementById('help').style.display='block';
+	document.getElementById('close-help-button').style.display='block';
+	document.getElementById('help-button').style.display='none';
+}
+
+function closeHelp() {
+	document.getElementById('help').style.display='none';
+	document.getElementById('close-help-button').style.display='none';
+	document.getElementById('help-button').style.display='block';
+}
