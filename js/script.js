41ffdf8f3ae195d3825a6920caa9ca7478a2fd87
João Monteiro <joaomiguelmonteiro@gmail.com>
2015-11-14 19:20:58 +0200

    Finished areas

--- js/script.js
+++ js/script.js
@@ -9,3 +9,51 @@ $('#help').on('show.bs.modal', function () {
 $('#help').on('hide.bs.modal', function () {
    $('#content').removeClass('blur');
 })
+
+$('#lisboa').on('show.bs.modal', function () {
+   $('#content').addClass('blur');
+})
+
+$('#lisboa').on('hide.bs.modal', function () {
+   $('#content').removeClass('blur');
+})
+
+$('#lagos').on('show.bs.modal', function () {
+   $('#content').addClass('blur');
+})
+
+$('#lagos').on('hide.bs.modal', function () {
+   $('#content').removeClass('blur');
+})
+
+$('#açores').on('show.bs.modal', function () {
+   $('#content').addClass('blur');
+})
+
+$('#açores').on('hide.bs.modal', function () {
+   $('#content').removeClass('blur');
+})
+
+$('#porto').on('show.bs.modal', function () {
+   $('#content').addClass('blur');
+})
+
+$('#porto').on('hide.bs.modal', function () {
+   $('#content').removeClass('blur');
+})
+
+$('#aveiro').on('show.bs.modal', function () {
+   $('#content').addClass('blur');
+})
+
+$('#aveiro').on('hide.bs.modal', function () {
+   $('#content').removeClass('blur');
+})
+
+$('#alentejo').on('show.bs.modal', function () {
+   $('#content').addClass('blur');
+})
+
+$('#alentejo').on('hide.bs.modal', function () {
+   $('#content').removeClass('blur');
+})
