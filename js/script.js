7360fe83e5014e815adbe28776f9d80040d679b8
João Monteiro <joaomiguelmonteiro@gmail.com>
2015-12-06 19:06:43 +0200

    Some bug-fixing

--- js/script.js
+++ js/script.js
@@ -13,7 +13,9 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    if ( ev.target.nodeName !== "IMG" ) {
+ 	   ev.target.appendChild(document.getElementById(data));
+	}
 }
 
 $(window).load(function(){
