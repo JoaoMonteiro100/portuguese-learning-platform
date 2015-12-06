2526ab3918e76584709bec6e75cf1820250a49e5
João Monteiro <joaomiguelmonteiro@gmail.com>
2015-12-06 17:05:12 +0200

    Drag & Drop v1

--- js/script.js
+++ js/script.js
@@ -184,3 +184,13 @@ function selectInovador2() {
 function selectInovador3() {
 	$('#inovador-3').addClass('green-cell');
 };
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
